687532892b525879700bc7b68a2c5b88687cc2cc
Akylbek <hsplay74@gmail.com>
2024-07-03 03:11:25 +0600

    added routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,12 @@
 import './App.css'
 import NavBar from "./components/NavBar/NavBar";
 import {Container} from "@mui/material";
+import {Route, Routes} from "react-router-dom";
+import Home from "./containers/Home/Home";
+import NewPost from "./containers/NewPost/NewPost";
+import Post from "./containers/Post/Post";
+import About from "./containers/About/About";
+import Contacts from "./containers/contacts/Contacts";
 
 const App = () => {
     return (
@@ -10,7 +16,15 @@ const App = () => {
                 <NavBar />
             </header>
                 <Container component="main">
-
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/posts" element={<Home />}></Route>
+                        <Route path="/posts/:id" element={<Post />} />
+                        <Route path="/posts/:id/edit" element={<NewPost />} />
+                        <Route path="posts/new-post" element={<NewPost />} />
+                        <Route path="/about" element={<About />} />
+                        <Route path="/contacts" element={<Contacts />} />
+                    </Routes>
                 </Container>
         </>
     );
