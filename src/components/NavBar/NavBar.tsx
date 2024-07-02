2d2a61b93a344dc6f9e3d6c32713f251a7d1583c
Akylbek <hsplay74@gmail.com>
2024-07-03 03:10:53 +0600

    added navigation buttons

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,13 +1,18 @@
-import {AppBar, Toolbar, Typography} from "@mui/material";
+import {AppBar, Button, Toolbar, Typography} from "@mui/material";
 import {NavLink} from "react-router-dom";
 
 const NavBar = () => {
+
     return (
         <AppBar position="static">
             <Toolbar>
                 <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                    <NavLink to="/">Blog</NavLink>
+                    <NavLink to="/" style={{ textDecoration: 'none', color: 'inherit' }}>Blog</NavLink>
                 </Typography>
+                <Button component={NavLink} to="/posts" sx={{color:'inherit'}}>Home</Button>
+                <Button component={NavLink} to="posts/new-post" sx={{color:'inherit'}}>Add Blog</Button>
+                <Button component={NavLink} to="/about" sx={{color:'inherit'}}>About</Button>
+                <Button component={NavLink} to="/contacts" sx={{color:'inherit'}}>Contacts</Button>
             </Toolbar>
         </AppBar>
     );
